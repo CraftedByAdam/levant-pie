50c8538bce879d542b7a5062d71592fb3e7359bd
Your Name <youremail@example.com>
2026-05-22 15:02:01 -0400

    I made a constructor 'Cheese(String toppingName, boolean isExtra)' and added the 'getPrice' method and override it so its custom to this class. I declared a double price to 0 and checked if the size is 8 inches then the price would be 0.75 cents, I did this for 2 other sizes and I also checked if they want extra to add however much it will be for that size to the aboriginal amount. And at the end I just returns the price.

--- src/main/java/com/pluralsight/pantry/Cheese.java
+++ src/main/java/com/pluralsight/pantry/Cheese.java
@@ -1,4 +1,30 @@
 package com.pluralsight.pantry;
 
 public class Cheese extends PremiumTopping{
+
+    public Cheese(String toppingName, boolean isExtra) {
+        super(toppingName, isExtra);
+    }
+
+    //Make better Adam!
+    @Override
+    public double getPrice(String size) {
+        double price = 0;
+        if (size.equals("8")) {
+            price = 0.75;
+        }else if (size.equals("12")) {
+            price = 1.50;
+        }else if (size.equals("16")) {
+            price = 2.25;
+        }
+        //Extra
+        if (isExtra() && size.equals("8")) {
+            price = 0.75 + 0.30;
+        }else if (isExtra() && size.equals("12")) {
+            price = 1.50 + 0.60;
+        }else if (isExtra() && size.equals("16")) {
+            price = 2.25 + 0.90;
+        }
+        return price;
+    }
 }
