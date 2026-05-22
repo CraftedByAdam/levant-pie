2b5646cf12329ee6007914f9a2b2fa70257d98c1
Your Name <youremail@example.com>
2026-05-22 14:03:52 -0400

    I added a constructor for 'Sauce' and the parameters are (String toppingName). I also made an @Override method 'getPrice' that returns 0 since the sauce toppings are included at no cost.

--- src/main/java/com/pluralsight/pantry/Sauce.java
+++ src/main/java/com/pluralsight/pantry/Sauce.java
@@ -1,4 +1,13 @@
 package com.pluralsight.pantry;
 
 public class Sauce extends Topping{
+
+    public Sauce(String toppingName) {
+        super(toppingName);
+    }
+
+    @Override
+    public double getPrice(String size) {
+        return 0;
+    }
 }
