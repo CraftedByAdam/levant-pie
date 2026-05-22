cbe96aa3678d83de0d65a713dc069e7eca455359
Your Name <youremail@example.com>
2026-05-22 14:06:13 -0400

    I added a constructor for 'Side' and the parameters are (String toppingName). I also made an @Override method 'getPrice' that returns 0 since the side toppings are included at no cost.

--- src/main/java/com/pluralsight/pantry/Side.java
+++ src/main/java/com/pluralsight/pantry/Side.java
@@ -1,4 +1,12 @@
 package com.pluralsight.pantry;
 
 public class Side extends Topping{
+    public Side(String toppingName) {
+        super(toppingName);
+    }
+
+    @Override
+    public double getPrice(String size) {
+        return 0;
+    }
 }
