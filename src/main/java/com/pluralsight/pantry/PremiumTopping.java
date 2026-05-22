aef73ba20bda7ef64ceff883a6cc2ebf46b84020
Your Name <youremail@example.com>
2026-05-22 14:56:08 -0400

    I added a boolean 'isExtra' attribute. Created a 'PremiumTopping' constructor that takes 'toppingName' and 'isExtra' as parameters. Made an 'isExtra' method that returns 'isExtra'. Then I made an abstract method 'getPrice(String size)'.

--- src/main/java/com/pluralsight/pantry/PremiumTopping.java
+++ src/main/java/com/pluralsight/pantry/PremiumTopping.java
@@ -1,4 +1,17 @@
 package com.pluralsight.pantry;
 
 public abstract class PremiumTopping extends Topping{
+
+    private boolean isExtra;
+
+    public PremiumTopping(String toppingName,boolean isExtra) {
+        super(toppingName);
+        this.isExtra = isExtra;
+    }
+
+    public boolean isExtra() {
+        return isExtra;
+    }
+
+    public abstract double getPrice(String size);
 }
