b1190f90a2fbae33bd850b90364ac5901bee8aca
Your Name <youremail@example.com>
2026-05-23 18:13:13 -0400

    I made a 'flavor' and 'size' attribute, I made a constructor 'Drink(String flavor, String size)'. Added the 'getFlavor()' and 'getSize()' method that returns flavor and size, then I override the 'getPrice()' method and check if the size is equally to "S" or "Small" and return 2.00, I did the same thing for medium and large but return their own prices, last I make a 'toString' that returns "Drink " + flavor + ", Size " + size + ", $" + getPrice()"

--- src/main/java/com/pluralsight/kitchen/Drink.java
+++ src/main/java/com/pluralsight/kitchen/Drink.java
@@ -1,4 +1,38 @@
 package com.pluralsight.kitchen;
 
 public class Drink implements IPayable{
+
+    private String flavor;
+    private String size;
+
+    public Drink(String flavor, String size) {
+        this.flavor = flavor;
+        this.size = size;
+    }
+
+    public String getFlavor() {
+        return flavor;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    @Override
+    public double getPrice() {
+        double price = 0;
+        if (size.equalsIgnoreCase("S") || size.equalsIgnoreCase("Small")) {
+            price = 2.00;
+        }else if (size.equalsIgnoreCase("M") || size.equalsIgnoreCase("Medium")) {
+            price = 2.50;
+        }else if (size.equalsIgnoreCase("L") || size.equalsIgnoreCase("Large")) {
+            price = 3.00;
+        }
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Drink " + flavor + ", Size " + size + ", $" + getPrice();
+    }
 }
