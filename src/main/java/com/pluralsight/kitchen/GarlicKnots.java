e66b14b1f0e57b6f9e561958918090e4c3181169
Your Name <youremail@example.com>
2026-05-23 17:51:34 -0400

    I made a 'flavor' attribute, I made a constructor 'GarlicKnots(String flavor)'. Added the 'getFlavor()' method that returns flavor, then I override the 'getPrice()' method and return 1.50 because the Knots stay at a fixed price, last I make a 'toString' that returns "Garlic Knots + getFlavor() + " $" + getPrice()""

--- src/main/java/com/pluralsight/kitchen/GarlicKnots.java
+++ src/main/java/com/pluralsight/kitchen/GarlicKnots.java
@@ -1,4 +1,24 @@
 package com.pluralsight.kitchen;
 
 public class GarlicKnots implements IPayable{
+
+    private String flavor;
+
+    public GarlicKnots(String flavor) {
+        this.flavor = flavor;
+    }
+
+    public String getFlavor() {
+        return flavor;
+    }
+
+    @Override
+    public double getPrice() {
+        return 1.50;
+    }
+
+    @Override
+    public String toString() {
+        return "Garlic Knots "  + getFlavor() + " $" + getPrice();
+    }
 }
