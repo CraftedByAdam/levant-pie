00ebd291e7259d686173adece2edc20b0f63896f
Your Name <youremail@example.com>
2026-05-25 17:47:09 -0400

    I Made a 'writeReceipt(Order order)' method, inside the method I made a DateTimeFormatter that formats the date like this ("yyyyMMdd-hhmmss"), then I created a variable 'formattedDateTime' and set it to the dateTimeFormatter I made that formats the date how I want it, then another variable 'fileName' and put together the folder path "receipts/" the formattedDateTime and the ".txt". After that I created a Try with resource and implemented a 'BufferedWriter' and a new 'FileWriter' and called the fileName I made and appended it false so it would creat a new one each time a customer makes an order. I write the name of my Pizza shop, then make a loop that goes over the ordered items and displays it one by one, then I write the "Total: $" plus the calculated total I made in the 'Order' class, then I close the try with a catch for any errors.

--- src/main/java/com/pluralsight/data/ReceiptFileManager.java
+++ src/main/java/com/pluralsight/data/ReceiptFileManager.java
@@ -1,4 +1,39 @@
 package com.pluralsight.data;
 
+import com.pluralsight.kitchen.IPayable;
+import com.pluralsight.kitchen.Order;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.time.format.DateTimeFormatter;
+
 public class ReceiptFileManager {
+
+    public void writeReceipt(Order order){
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyyMMdd-hhmmss");
+
+       String formattedDateTime;
+       formattedDateTime = dateTimeFormatter.format(order.getDateTime());
+
+        String fileName = "receipts/" + formattedDateTime + ".txt";
+
+        try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName, false))) {
+            bufferedWriter.write("-------Levant Pie-------");
+            bufferedWriter.newLine();
+
+            for (IPayable item : order.getItems() ) {
+                bufferedWriter.write(item.toString());
+                bufferedWriter.newLine();
+            }
+
+            bufferedWriter.write("Total: $" + order.calculateTotal());
+            bufferedWriter.newLine();
+
+            bufferedWriter.write("-------------------------------");
+            bufferedWriter.newLine();
+
+        }catch (Exception e) {
+            System.out.println("Error writing receipt file");
+        }
+    }
 }
