5dcc40e7e8a940ff6a524e3866a07fdca8435fc4
Your Name <youremail@example.com>
2026-05-25 12:18:34 -0400

    I made a 'crustType', 'isStuffed', 'size' attribute, and a List<Topping>topping to hold the toppings. I made a constructor and getters for each attribute. Added the 'addTopping(Topping topping) method, then I override the 'getPrice()' method and creat a variable 'totalPrice' and set it to 0, then check if the size is equally to "8" and set 'totalPrice' to 8.50, I did the same thing for "12" and "16" but return their own prices. Then I make an if statement for if the pizza is stuffed, and if it is then I add 3.20 to the total price, then I loop over the toppings and get the price and size and add it to the totalPrice. last I make a 'toString' that returns "crustType + isStuffed + size + toppings + getPrice()"

--- src/main/java/com/pluralsight/kitchen/Pizza.java
+++ src/main/java/com/pluralsight/kitchen/Pizza.java
@@ -1,4 +1,64 @@
 package com.pluralsight.kitchen;
 
+import com.pluralsight.pantry.Topping;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pizza implements IPayable{
+    private String crustType;
+    private boolean isStuffed;
+    private String size;
+    List<Topping>toppings;
+
+    public Pizza(String crustType, boolean isStuffed, String size) {
+        this.crustType = crustType;
+        this.isStuffed = isStuffed;
+        this.size = size;
+        this.toppings = new ArrayList<>();
+    }
+
+    public String getCrustType() {
+        return crustType;
+    }
+
+    public boolean isStuffed() {
+        return isStuffed;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public List<Topping> getToppings() {
+        return toppings;
+    }
+
+    public void addTopping(Topping topping) {
+        toppings.add(topping);
+    }
+
+    @Override
+    public double getPrice() {
+        double totalPrice = 0;
+        if (size.equals("8")) {
+            totalPrice = 8.50;
+        } else if (size.equals("12")) {
+            totalPrice = 12.00;
+        } else if (size.equals("16")) {
+            totalPrice = 16.50;
+        }
+        if (isStuffed) {
+            totalPrice += 3.20;
+        }
+        for (Topping topping : toppings) {
+            totalPrice += topping.getPrice(size) ;
+        }
+        return totalPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Pizza: " + "crustType: " + crustType + ", isStuffed: " + isStuffed + ", size: " + size + ", toppings: " + toppings + "Price: $" + getPrice();
+    }
 }
