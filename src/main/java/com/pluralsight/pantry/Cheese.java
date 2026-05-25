659a8c0e476e54cc4908d5cea4327f7131a93bce
Your Name <youremail@example.com>
2026-05-25 12:28:46 -0400

    I removed the repeated if/else if statements for if its extra and placed them inside the if conditions that check what size it is so it will only check what size it is once.

--- src/main/java/com/pluralsight/pantry/Cheese.java
+++ src/main/java/com/pluralsight/pantry/Cheese.java
@@ -6,24 +6,24 @@ public class Cheese extends PremiumTopping{
         super(toppingName, isExtra);
     }
 
-    //Make better Adam!
     @Override
     public double getPrice(String size) {
         double price = 0;
         if (size.equals("8")) {
             price = 0.75;
+            if (isExtra()) {
+                price += 0.30;
+            }
         }else if (size.equals("12")) {
             price = 1.50;
+            if (isExtra()) {
+                price += 0.60;
+            }
         }else if (size.equals("16")) {
             price = 2.25;
-        }
-        //Extra
-        if (isExtra() && size.equals("8")) {
-            price = 0.75 + 0.30;
-        }else if (isExtra() && size.equals("12")) {
-            price = 1.50 + 0.60;
-        }else if (isExtra() && size.equals("16")) {
-            price = 2.25 + 0.90;
+            if (isExtra()) {
+                price += 0.90;
+            }
         }
         return price;
     }
