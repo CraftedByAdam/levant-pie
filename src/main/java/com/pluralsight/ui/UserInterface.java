284612bbe322612626afba017a0fea03f82ed5fb
Your Name <youremail@example.com>
2026-05-26 11:47:54 -0400

    I built the 'displayOrderScreen()' method, I crated a boolean running and set it to true, then made a while loop and inside I promoted the user options and then asked them to enter their choice. I used a switch case to go through what the user enters, so if the user enters "1" it will take them to the 'addPizza method' where they will be prompted to enter details, I did this for 'addDrink()', 'addGarlicKnots()', and 'checkout()' methods. I also made one for if the user want to cancel the order they enter "5" and it closes the loop and sends them back to the home screen, and last I made a default switch that catches any errors and if it does it lets the user know and lets them try again.

--- src/main/java/com/pluralsight/ui/UserInterface.java
+++ src/main/java/com/pluralsight/ui/UserInterface.java
@@ -34,7 +34,7 @@ public class UserInterface {
         boolean running = true;
         while(running){
             System.out.println(BLUE + "1) New Order🛍️" + RESET);
-            System.out.println(RED + "0) Exit👋" + RESET);
+            System.out.println(RED + "0) Exit👋\n" + RESET);
 
             System.out.print(WHITE + "Enter your choice: " + RESET);
             String choice = scanner.nextLine();
@@ -49,12 +49,45 @@ public class UserInterface {
                     System.out.println("\nThanks For Visiting!😁");
                     break;
                 default:
-                    System.out.println("\n❌Invalid choice try again🔄️");
+                    System.out.println("\n❌Invalid choice try again🔄️\n");
             }
         }
     }
 
-    public void displayOrderScreen(){}
+    public void displayOrderScreen(){
+        boolean running = true;
+
+        while(running){
+            System.out.println("\n1) Add Pizza🍕");
+            System.out.println("2) Add Drink🥤");
+            System.out.println("3) Add Garlic Knots🧄🍞");
+            System.out.println("4) Checkout✅");
+            System.out.println("5) Cancel Order❌\n");
+
+            System.out.print(WHITE + "Enter your choice: " + RESET);
+            String choice = scanner.nextLine();
+
+            switch(choice){
+                case "1":
+                    addPizza();
+                    break;
+                case "2":
+                    addDrink();
+                    break;
+                case "3":
+                    addGarlicKnots();
+                    break;
+                case "4":
+                    checkout();
+                    break;
+                case "5":
+                    running = false;
+                    break;
+                default:
+                    System.out.println("\n❌Invalid choice try again🔄️");
+            }
+        }
+    }
 
     public void addPizza(){}
 
