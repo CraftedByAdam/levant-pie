5237f6795c15edc61ba41156805c79dd114c0eaf
Your Name <youremail@example.com>
2026-05-26 10:39:39 -0400

    I initialized the 'UserInterface' in the main 'LevantPie' and called the home screen method so I can run the app and test as I make changes and add features.

--- src/main/java/com/pluralsight/LevantPie.java
+++ src/main/java/com/pluralsight/LevantPie.java
@@ -1,4 +1,12 @@
 package com.pluralsight;
 
+
+import com.pluralsight.ui.UserInterface;
+
 public class LevantPie {
+    public static void main(String[] args) {
+
+        UserInterface userInterface = new UserInterface();
+        userInterface.displayHomeScreen();
+    }
 }
