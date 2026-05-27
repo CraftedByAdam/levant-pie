69021ca8869b73465f3e75f194ea58629d98277c
Your Name <youremail@example.com>
2026-05-27 13:10:18 -0400

    I updated the 'displayOrder()' method and I used StringBuilder to display the receipt nicely and instead of printing the order line by line, I also added a DateTimeFormatter to display the live date properly.

--- src/main/java/com/pluralsight/levantpie/order/Order.java
+++ src/main/java/com/pluralsight/levantpie/order/Order.java
@@ -1,12 +1,16 @@
 package com.pluralsight.levantpie.order;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Order{
     private LocalDateTime dateTime;
     private List<IPayable> items;
+    DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("MM/dd/yyyy hh:mm:ss");
+
+
 
     public Order(){
         this.dateTime = LocalDateTime.now();
@@ -34,10 +38,20 @@ public class Order{
     }
 
     public void displayOrder(){
-        //Make a string builder
-        //test it
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("\tLevant Pie\n");
+        stringBuilder.append(" ____________________\n");
+        stringBuilder.append("|      Receipt       |\n");
+        stringBuilder.append("|____________________|\n\n");
+        stringBuilder.append("Date: ").append(dateTimeFormatter.format(dateTime)).append("\n\n");
+        stringBuilder.append("Order Summary:\n");
+        stringBuilder.append("==============================\n");
+
         for (IPayable item : items){
-            System.out.println(item.toString());
+            stringBuilder.append(item.toString()).append("\n");
         }
+        stringBuilder.append("==============================\n");
+        stringBuilder.append("Total: $").append(String.format("%.2f", calculateTotal()));
+        System.out.println(stringBuilder.toString());
     }
 }
