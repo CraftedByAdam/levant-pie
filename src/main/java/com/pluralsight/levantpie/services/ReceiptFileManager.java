6298e44426687a01ae7013f456c52844c7d0e276
Your Name <youremail@example.com>
2026-05-27 13:12:17 -0400

    I took what I did in the 'Order' class for my 'displayOrder()' method and added it inside the BufferedWriter I made so it matches the receipt that would be shown in the console.

--- src/main/java/com/pluralsight/levantpie/services/ReceiptFileManager.java
+++ src/main/java/com/pluralsight/levantpie/services/ReceiptFileManager.java
@@ -18,19 +18,22 @@ public class ReceiptFileManager {
         String fileName = "receipts/" + formattedDateTime + ".txt";
 
         try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName, false))) {
-            bufferedWriter.write("-------Levant Pie-------");
-            bufferedWriter.newLine();
 
-            for (IPayable item : order.getItems() ) {
-                bufferedWriter.write(item.toString());
-                bufferedWriter.newLine();
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append("\tLevant Pie\n");
+            stringBuilder.append(" ____________________\n");
+            stringBuilder.append("|      Receipt       |\n");
+            stringBuilder.append("|____________________|\n\n");
+            stringBuilder.append("Date: ").append(dateTimeFormatter.format(order.getDateTime())).append("\n\n");
+            stringBuilder.append("Order Summary:\n");
+            stringBuilder.append("==============================\n");
+
+            for (IPayable item : order.getItems()){
+                stringBuilder.append(item.toString()).append("\n");
             }
-
-            bufferedWriter.write("Total: $" + order.calculateTotal());
-            bufferedWriter.newLine();
-
-            bufferedWriter.write("-------------------------------");
-            bufferedWriter.newLine();
+            stringBuilder.append("==============================\n");
+            stringBuilder.append("Total: $").append(String.format("%.2f", order.calculateTotal()));
+            bufferedWriter.write(stringBuilder.toString());
 
         }catch (Exception e) {
             System.out.println("Error writing receipt file");
